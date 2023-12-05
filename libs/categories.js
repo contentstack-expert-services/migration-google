73529de4cebd9f07738de637073a75e0ee5148bb
umeshmore45 <umesh.more@contentstack.com>
2023-12-06 00:54:50 +0530

    refactor code

--- libs/categories.js
+++ libs/categories.js
@@ -22,22 +22,23 @@ function categories() {
       const entry = {};
       const file = helper?.readFile({ path: `${globalFolder}/${item}` })
       entry.uid = file?.categoryId?.replace?.(/-/g, '');
-      entry.category_display_name = rteMapper({ type: "doc" });
-      entry.category_display_name.children = [rteMapper({ type: "paragraph", text: file?.source?.category?.displayName })]
-      entry.title = file?.source?.category?.displayName;
+      entry.sdp_category_display_name = rteMapper({ type: "doc" });
+      entry.sdp_category_display_name.children = [rteMapper({ type: "paragraph", text: file?.source?.category?.displayName })]
+      entry.title = `${file?.source?.category?.displayName} ${entry.uid} `;
       if (file?.parentCategoryId) {
-        entry.sdp_category_parent = [
-          {
-            "uid": file?.parentCategoryId?.replace?.(/-/g, ''),
-            "_content_type_uid": "sdp_categories"
-          }
-        ]
+        entry.sdp_category_parent = []
+        // {
+        //   "uid": file?.parentCategoryId?.replace?.(/-/g, ''),
+        //     "_content_type_uid": "sdp_categories"
+        // }
       } else {
         entry.sdp_category_parent = []
       }
       if (file?.source?.category?.description?.length) {
-        entry.category_description = { json_rte: objectNester(file?.source?.category?.description) };
-      }
+        const sdp_main_json_rte = rteMapper({ type: "doc" });
+        sdp_main_json_rte.children = objectNester(file?.source?.category?.description);
+        entry.sdp_category_description = { sdp_main_json_rte }
+      };
       helper.handleFile({ locale: "en-us", contentType: "sdp_categories", entry, uid: entry?.uid })
     }
   })
